916ef09a9f2436671fd80260fb88c6af10c971cf
Marko Korhonen <marko@korhonen.cc>
2022-06-12 02:23:32 +0300

    Update key fingerprint

--- content/korhonen_aur/pacman-commands.sh
+++ content/korhonen_aur/pacman-commands.sh
@@ -1,2 +1,2 @@
 curl 'https://korhonen.cc/pgp/marko_korhonen_gpg.asc' | sudo pacman-key -a -
-sudo pacman-key --lsign-key E40A9DD549AA7A3CFCCBBC3D911B85FBC6003FE5
+sudo pacman-key --lsign-key 1EFC4DBB079407113A761DF7C514037F1EFF4FA6
