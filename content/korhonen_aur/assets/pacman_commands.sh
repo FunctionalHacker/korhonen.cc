8d25a5673b30cb64be8db0e34e5dce76ff8a86ca
Marko Korhonen <marko@korhonen.cc>
2022-08-28 20:14:57 +0300

    korhonen_aur pacman-key commands as root instead of sudo

--- content/korhonen_aur/assets/pacman_commands.sh
+++ content/korhonen_aur/assets/pacman_commands.sh
@@ -1,2 +1,3 @@
-curl 'https://korhonen.cc/pgp/assets/marko_korhonen_gpg.asc' | sudo pacman-key -a -
-sudo pacman-key --lsign-key F2B9F713E7ED627336DD85E444FA3F28CD931BB0
+sudo -i # Login as root
+curl 'https://korhonen.cc/pgp/assets/marko_korhonen_gpg.asc' | pacman-key -a -
+pacman-key --lsign-key F2B9F713E7ED627336DD85E444FA3F28CD931BB0
