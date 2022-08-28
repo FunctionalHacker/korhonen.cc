bda2d135130c92d4b3972d4ac415680e5d554413
Marko Korhonen <marko@korhonen.cc>
2022-08-28 20:13:10 +0300

    Update korhonen_aur instructions to the new GPG key

---
+++ content/korhonen_aur/assets/pacman_commands.sh
@@ -0,0 +1,2 @@
+curl 'https://korhonen.cc/pgp/assets/marko_korhonen_gpg.asc' | sudo pacman-key -a -
+sudo pacman-key --lsign-key F2B9F713E7ED627336DD85E444FA3F28CD931BB0
--- content/korhonen_aur/code_blocks/pacman_commands.sh
+++
@@ -1,2 +0,0 @@
-curl 'https://korhonen.cc/pgp/marko_korhonen_gpg.asc' | sudo pacman-key -a -
-sudo pacman-key --lsign-key 1EFC4DBB079407113A761DF7C514037F1EFF4FA6
