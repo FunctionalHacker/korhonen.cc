25753a5034da4a7df5c641a4e1c41b94d1bc39d1
Marko Korhonen <marko@korhonen.cc>
2023-03-01 08:47:54 +0200

    korhonen_aur: Use WKD to fetch pacman signing key

--- content/korhonen_aur/assets/pacman_commands.sh
+++ content/korhonen_aur/assets/pacman_commands.sh
@@ -1,3 +1,2 @@
-sudo -i # Login as root
-curl 'https://korhonen.cc/pgp/assets/marko_korhonen_pgp.asc' | pacman-key -a -
-pacman-key --lsign-key F2B9F713E7ED627336DD85E444FA3F28CD931BB0
+sudo pacman-key -r marko@korhonen.cc
+sudo pacman-key --lsign-key F2B9F713E7ED627336DD85E444FA3F28CD931BB0
